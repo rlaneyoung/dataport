f6284e790af7d33e45e20edce29a2e816ac4cee5
R. Lane Young <rlaneyoung12@gmail.com>
2017-08-11 20:18:47 -0400

    Formatting and comment changes
    More whitespace issues caused by tabs. Changed comments as well.

--- port.js
+++ port.js
@@ -4,7 +4,7 @@ const { EventEmitter } = require('events');
   * @param {String} name The name of the Port
   *
   * @extends EventEmitter
-	*/
+  */
 module.exports = class Port extends EventEmitter {
   constructor(name){
     super();
@@ -22,9 +22,9 @@ module.exports = class Port extends EventEmitter {
   * Function to set a new condition test. If routed data passes this test, it
   * triggers the function.
   *
-	* @param {Object|Function} condition The object or function to test received data with.
+  * @param {Object|Function} condition The object or function to test received data with.
   * @param {Function} handler The function to call if condition test passes.
-	*/
+  */
   set(condition, handler){
     const Port = this;
     Port.conditions.push({
@@ -34,12 +34,12 @@ module.exports = class Port extends EventEmitter {
   }
 
 /**
-	* Function to create a new data route. Data comes from route function, runs through condition
+  * Function to create a new data route. Data comes from route function, runs through condition
   * tests, then goes to destination function.
   *
   * @param {String} route Name for newly created route.
   * @param {Function} destination Final function to be called after all condition tests.
-	*/
+  */
   createRoute(route, destination){
     const Port = this;
     Port.routes[route] = { destination };
@@ -47,34 +47,30 @@ module.exports = class Port extends EventEmitter {
 
 
 /**
-	* Routing function for this dataport. Used as the 'input' function.
+  * Routing function for this dataport. Used as the 'input' function.
   *
   * @param {String} route The name of the target route.
   * @param {JSON} data Data to be parsed and then routed through target route.
-	*/
+  */
   route(route, data){
     const Port = this;
     Port.emit("launch", data);
 
   /**
-  	* Condition Tests - In case of Object, data must contain
+    * Condition Tests - In case of Object, data must contain
     * equivalent key value pairs to pass. In case of Function, function must return true.
     * If data passes a test, it is then processed by the handler function attached to that condition object.
-  	*/
+    */
     const conditions = Port.conditions;
 
     // First we filter out any condition tests that our data did not pass.
     conditions.filter(function(condition){
       let conditional = condition.condition;
 
-    /**
-    	* If condition test is a function, return output of function; true or false.
-    	* If condition test is an object, return true if all key values pairs present in
-      * condition are also found in provided data.
-      */
+      // If condition test is a function, return output of function; true or false.
       if(typeof conditional === "function"){
         return conditional(data);
-      }
+      } // Else If condition test is an object, return true if all key value pairs in data match
       else if(typeof conditional === "object"){
 
         let keys = Object.keys(conditional);
